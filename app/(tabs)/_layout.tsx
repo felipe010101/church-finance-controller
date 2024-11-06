6fe44bb3bd4356396a6979a5c78fac3c5db4bd09
Felipe Soares <65181945+felipe010101@users.noreply.github.com>
2024-11-06 11:39:40 -0300

    tela de cadastro

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -54,6 +54,13 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
         }}
       />
+      <Tabs.Screen
+        name="cadastro"
+        options={{
+          title: 'Cadastro',
+          tabBarIcon: ({ color }) => <TabBarIcon name="edit" color={color} />,
+        }}
+      />
     </Tabs>
   );
 }
